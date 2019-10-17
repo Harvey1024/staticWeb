0e27638df7465398840ec07e97ade875c35c143c
Harvey <hw-li@foxmail.com>
2019-10-17 21:18:52 +0800

    widget structure

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -1,16 +1,8 @@
-function c1showm(){
-    var w1=document.getElementById("x1");
-    w1.removeAttribute("class","x1hide");
+function c1showm(n) {
+    var w1 = document.getElementsByClassName("co");
+    w1[n].children[0].style.display = "block";
 }
-function c1hidem(){
-    var w1=document.getElementById("x1");
-    w1.setAttribute("class","x1hide");
-}
-function c2showm(){
-    var w1=document.getElementById("x2");
-    w1.removeAttribute("class","x2hide");
-}
-function c2hidem(){
-    var w1=document.getElementById("x2");
-    w1.setAttribute("class","x2hide");
+function c1hidem(n) {
+    var w1 = document.getElementsByClassName("co");
+    w1[n].children[0].style.display = "none";
 }
