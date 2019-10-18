accc79473626d7f3a420b081b78e645266a99f9a
Harvey <hw-li@foxmail.com>
2019-10-18 23:21:23 +0800

    add drop down medel

--- widget/js/widget.js
+++ widget/js/widget.js
@@ -6,3 +6,11 @@ function c1hidem(n) {
     var w1 = document.getElementsByClassName("co");
     w1[n].children[0].style.display = "none";
 }
+function showDropDown() {
+    document.getElementsByClassName("cDropDownContent")[0].classList.toggle("show");
+}
+window.onclick = function (event) {
+    if (!event.target.matches(".cDropBtn")) {
+        dropdown = document.getElementsByClassName("cDropDownContent")[0].classList.remove('show')
+    }
+}
